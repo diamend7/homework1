934d021c37c3c536d6ececfe368238956b245405
Vyacheslav <goodgamefnaf@gmail.com>
2024-03-04 21:40:11 +0200

    try1

--- scripy.js
+++ scripy.js
@@ -3,47 +3,15 @@ $(`#tab-home`).addClass(`activepar`);
 
 $(`.nav-link`).click(function (e) {
   e.preventDefault();
-  const thisbutton = e.target;
+  const thisbutton = this;
   const databutton = $(thisbutton).data(`target`);
-
-  const previousButton = $(".nav-link.activebutton");
-  if (previousButton) {
-    previousButton.removeClass("activebutton");
-  }
   console.log(databutton);
 
+  $(".nav-link").removeClass("activebutton");
+  $(".tab-pane").removeClass("activepar");
+
   $(databutton).addClass(`activebutton`);
-
-  if (databutton == `#nav-home`) {
-    const previousPar = $(".tab-pane.activepar");
-    if (previousPar) {
-      previousPar.removeClass("activepar");
-    }
-
-    $(`#tab-home`).addClass(`activepar`);
-  }
-  if (databutton == `#nav-profile`) {
-    const previousPar = $(".tab-pane.activepar");
-    if (previousPar) {
-      previousPar.removeClass("activepar");
-    }
-
-    $(`#tab-profile`).addClass(`activepar`);
-  }
-  if (databutton == `#nav-contact`) {
-    const previousPar = $(".tab-pane.activepar");
-    if (previousPar) {
-      previousPar.removeClass("activepar");
-    }
-
-    $(`#tab-contact`).addClass(`activepar`);
-  }
-  if (databutton == `#nav-delivery`) {
-    const previousPar = $(".tab-pane.activepar");
-    if (previousPar) {
-      previousPar.removeClass("activepar");
-    }
-
-    $(`#tab-delivery`).addClass(`activepar`);
-  }
+  $(databutton).addClass(`activepar`);
+  $(".nav-link").removeClass("activepar");
+  // я багато видаляв, нічого не працювало, це теж не працює як хотів через таргети
 });
