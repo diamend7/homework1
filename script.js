aef8fb4e3c0f37bdee153bb3f387d49026b7ea45
Vyacheslav <goodgamefnaf@gmail.com>
2024-03-11 22:29:02 +0200

    finalwork

--- script.js
+++ script.js
@@ -3,22 +3,19 @@ const smoke = confirm(`Are you smoke?`);
 let age = Number(prompt(`Enter your age`));
 
 console.log(isNaN(age));
-// if (isNaN(age) === true) {
-while (isNaN(age) === true || age === 0) {
+
+while (age >= 100 || age <= 1 || isNaN(age) === true || age === 0) {
   age = Number(prompt(`Enter your age`));
-  // alert(`Enter your age or i will find you F5 `);
 }
-// }
+
 console.log(age);
 
 let named = prompt(`Enter your name`);
 while (named === null || named.trim() === ``) {
-  // name = `Інвалід`;
   named = prompt(`Enter your name`);
 }
 
-// ,.больше
-if (age > 18) {
+if (age >= 18) {
   if (!smoke) {
     alert("Супер " + named + "! Мабуть ще й спортом займаєшся!");
   } else {
@@ -26,8 +23,7 @@ if (age > 18) {
   }
 }
 
-//менше
-if (age < 18) {
+if (age <= 17) {
   if (!smoke) {
     alert("Молодець " + named + "! Гарний приклад для своїх однолітків");
   } else {
