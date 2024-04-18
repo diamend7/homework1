5fcd5f86688da8e528cd6fc4cff891610209dbb6
Vyacheslav <goodgamefnaf@gmail.com>
2024-04-18 22:29:32 +0300

    contactpage

--- script.js
+++ script.js
@@ -6,3 +6,16 @@ $(`#jesuschrist`).click(function (e) {
   e.preventDefault();
   $(`#smallsizegridoptions`).removeClass(`active`);
 });
+
+const input = document.querySelectorAll(".JsRed");
+
+function validateForm() {
+  for (let i = 0; input.length > i; i++) {
+    console.log(input);
+    if (input[i].value === "") {
+      $(input[i]).css("border", "1px solid #f0145a");
+      $(input[i]).attr(":placeholder", "color: #f0145a");
+    } else {
+    }
+  }
+}
